d4b2550b92b321d62a8bc61483ee6f2eb6c0ffe2
Emmanuel Owusu <e_owusu@live.com>
2018-05-09 17:13:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Write your code in this file!
 
 const currentUser = "Maabena";
-const welcomeMessage = 'Welcome to Flatbook,  ${currentUser}';
+const welcomeMessage = `Welcome to Flatbook,  ${currentUser}!`;
