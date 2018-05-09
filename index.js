f45a291f2ce691568a7dd51b57cba32f89679903
Emmanuel Owusu <e_owusu@live.com>
2018-05-09 17:13:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,5 @@
 
 const currentUser = "Maabena";
 const welcomeMessage = `Welcome to Flatbook,  ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
